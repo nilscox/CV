0a78c38788f1b5c097e9c4b8ec5a62dbd5ec83dc
Nils Cox <nils@epita.fr>
2018-08-14 16:00:18 +0200

    update data loading logic

--- src/index.js
+++ src/index.js
@@ -3,23 +3,9 @@ import ReactDOM from 'react-dom';
 import CV from './CV';
 import registerServiceWorker from './registerServiceWorker';
 
-const queryString = window.location.search.replace(/^\?/, '');
-const params = queryString.split('&').reduce((o, a) => {
-  const p = a.split('=');
-
-  o[p[0]] = p[1];
-
-  return o;
-}, {});
-
-const lang =
-  params.lang
-  || process.env.REACT_APP_LANG
-  || 'en';
-
 const props = {
-  meta: require(`./data/${lang}/meta`),
-  data: require(`./data/${lang}/cv`),
+  meta: require(`./data/meta`),
+  data: require(`./data/cv`),
 };
 
 ReactDOM.render(<CV {...props} />, document.getElementById('root'));
