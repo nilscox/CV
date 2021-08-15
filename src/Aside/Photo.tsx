3093c5810a7909f9e2cc8cf95fa50d9e9b8cfdfd
Nils <nils@epita.fr>
2021-08-15 23:51:57 +0200

    remove all the code

--- src/Aside/Photo.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import photo from './photo.jpg';
-
-const Photo: React.FC = () => {
-  return <img src={photo} alt="Hello! =)" className="photo" />;
-};
-
-export default Photo;
